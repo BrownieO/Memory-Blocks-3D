ceff9ddc259dc67dcb06ed57312a53aaa0b6730d
BrownieO <thebrainyanimator@gmail.com>
2024-11-07 23:56:45 -0300

    Adjusted camera distance

--- memorice.js
+++ memorice.js
@@ -16,7 +16,7 @@ const createScene = function() {
 
     //Camera
     const scene = new BABYLON.Scene(engine);
-    const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, 0, 300, center, scene);
+    const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2, 0, 270, center, scene);
     //camera.attachControl(canvas, true);
 
     //camera.upperBetaLimit = (Math.PI / 2) * 0.99;
