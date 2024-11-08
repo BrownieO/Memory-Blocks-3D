f2e0f1c5448f625aa784be3254e5f75e1b3f7a60
BrownieO <thebrainyanimator@gmail.com>
2024-11-07 21:56:43 -0300

    Disable camera controls

--- memorice.js
+++ memorice.js
@@ -18,7 +18,7 @@ const createScene = function () {
 	//Camera
     const scene = new BABYLON.Scene(engine);
     const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI/2, 0, 300, center, scene);
-    camera.attachControl(canvas, true);
+    //camera.attachControl(canvas, true);
 
     //camera.upperBetaLimit = (Math.PI / 2) * 0.99;
 
